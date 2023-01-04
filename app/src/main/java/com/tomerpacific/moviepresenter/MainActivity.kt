6a939d0622e055025e6c934688e62cf285dc3a09
unknown <tomerpacific@gmail.com>
2023-01-04 22:35:47 +0200

    MoviesPresenter creating initial layout with title text

--- app/src/main/java/com/tomerpacific/moviepresenter/MainActivity.kt
+++ app/src/main/java/com/tomerpacific/moviepresenter/MainActivity.kt
@@ -3,13 +3,17 @@ package com.tomerpacific.moviepresenter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.tomerpacific.moviepresenter.ui.theme.MoviePresenterTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +26,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        Row ( Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Text(text = "Movie Presenter", fontSize = 30.sp, fontWeight = FontWeight.Bold)
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MoviePresenterTheme {
-        Greeting("Android")
     }
 }
