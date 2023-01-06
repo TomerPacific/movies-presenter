0b5475aed70b119b404005934eacdf9d23bb96c2
unknown <tomerpacific@gmail.com>
2023-01-06 22:51:57 +0200

    MoviesPresenter creating constants class

---
+++ app/src/main/java/com/tomerpacific/moviepresenter/Constants.kt
@@ -0,0 +1,6 @@
+package com.tomerpacific.moviepresenter
+
+object Constants {
+    const val TMDB_META_DATA_KEY = "TMDB"
+    const val ENDPOINT = "https://api.themoviedb.org/3/movie/popular?api_key="
+}
