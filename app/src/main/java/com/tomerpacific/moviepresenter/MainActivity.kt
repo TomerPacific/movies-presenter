fe7341b564ba34bfe6476e519011a40c03775c69
unknown <tomerpacific@gmail.com>
2023-01-07 20:51:43 +0200

    MoviesPresenter logic to display list of movies and using viewmodel

--- app/src/main/java/com/tomerpacific/moviepresenter/MainActivity.kt
+++ app/src/main/java/com/tomerpacific/moviepresenter/MainActivity.kt
@@ -3,25 +3,30 @@ package com.tomerpacific.moviepresenter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.horizontalScroll
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.tomerpacific.moviepresenter.model.MainViewModel
 import com.tomerpacific.moviepresenter.ui.theme.MoviePresenterTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+
             MoviePresenterTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
@@ -32,7 +37,21 @@ class MainActivity : ComponentActivity() {
                         ) {
                             Text(text = "Movie Presenter", fontSize = 30.sp, fontWeight = FontWeight.Bold)
                         }
+                        MoviesList()
                     }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun MoviesList() {
+        val movies = viewModel.moviesList.value
+
+        LazyColumn {
+            items(movies) { movie ->
+                Row {
+                   Text(text = movie.title, fontSize = 20.sp, fontWeight = FontWeight.Bold)
                 }
             }
         }
