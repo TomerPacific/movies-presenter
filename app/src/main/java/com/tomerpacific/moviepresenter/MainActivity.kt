4f635ba0e0c95787c3775ca509863700c93ac4b2
unknown <tomerpacific@gmail.com>
2023-01-08 21:10:44 +0200

    MoviesPresenter logic to show movie in list with movie poster

--- app/src/main/java/com/tomerpacific/moviepresenter/MainActivity.kt
+++ app/src/main/java/com/tomerpacific/moviepresenter/MainActivity.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tomerpacific.moviepresenter.model.MainViewModel
 import com.tomerpacific.moviepresenter.ui.theme.MoviePresenterTheme
@@ -48,10 +55,25 @@ class MainActivity : ComponentActivity() {
     fun MoviesList() {
         val movies = viewModel.moviesList.value
 
-        LazyColumn {
+        LazyColumn(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             items(movies) { movie ->
-                Row {
-                   Text(text = movie.title, fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                Card(
+                    border = BorderStroke(2.dp, Color.Cyan),
+                    modifier = Modifier.fillMaxWidth().padding(bottom = 5.dp)
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    )
+                    {
+                        Text(modifier = Modifier.padding(5.dp),
+                            text = movie.originalTitle, fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                        movie.posterImgBitmap?.let {
+                            Image(bitmap = it.asImageBitmap(), contentDescription = "Movie Poster")
+                        }
+
+                    }
                 }
             }
         }
