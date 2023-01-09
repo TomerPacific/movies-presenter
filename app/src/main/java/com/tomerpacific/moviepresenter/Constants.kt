96d3f789bcd199924013c4ad20e7449d9261da98
unknown <tomerpacific@gmail.com>
2023-01-09 21:50:25 +0200

    MoviesPresenter adding constant for app title

--- app/src/main/java/com/tomerpacific/moviepresenter/Constants.kt
+++ app/src/main/java/com/tomerpacific/moviepresenter/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
     const val TMDB_META_DATA_KEY = "TMDB"
     const val POPULAR_MOVIES_ENDPOINT = "https://api.themoviedb.org/3/movie/popular?api_key="
     const val MOVIE_POSTER_ENDPOINT = "https://image.tmdb.org/t/p/w200/"
+    const val APP_TITLE = "Movie Presenter"
 }
