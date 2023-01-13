6db47e813dc8e3cbd91c81ec1e659abbd244bd19
unknown <tomerpacific@gmail.com>
2023-01-13 09:23:33 +0200

    MoviePresenter breaking down endpoint for images to account for small/large sizes

--- app/src/main/java/com/tomerpacific/moviepresenter/Constants.kt
+++ app/src/main/java/com/tomerpacific/moviepresenter/Constants.kt
@@ -3,6 +3,8 @@ package com.tomerpacific.moviepresenter
 object Constants {
     const val TMDB_META_DATA_KEY = "TMDB"
     const val POPULAR_MOVIES_ENDPOINT = "https://api.themoviedb.org/3/movie/popular?api_key="
-    const val MOVIE_POSTER_ENDPOINT = "https://image.tmdb.org/t/p/w200/"
+    const val MOVIE_POSTER_ENDPOINT = "https://image.tmdb.org/t/p/"
+    const val MOVIE_POSTER_SMALL_SIZE = "w200/"
+    const val MOVIE_POSTER_LARGE_SIZE = "w500/"
     const val APP_TITLE = "Movie Presenter"
 }
