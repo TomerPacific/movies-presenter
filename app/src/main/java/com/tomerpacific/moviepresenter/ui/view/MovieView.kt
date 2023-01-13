cd273ffaafe245680811d5c5e1aad9bccc37d10f
unknown <tomerpacific@gmail.com>
2023-01-13 10:33:42 +0200

    MoviePresenter adding progress bar and using state

--- app/src/main/java/com/tomerpacific/moviepresenter/ui/view/MovieView.kt
+++ app/src/main/java/com/tomerpacific/moviepresenter/ui/view/MovieView.kt
@@ -1,12 +1,11 @@
 package com.tomerpacific.moviepresenter.ui.view
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
@@ -15,31 +14,40 @@ import com.tomerpacific.moviepresenter.model.MainViewModel
 @Composable
 fun MovieView(viewModel: MainViewModel) {
 
-    viewModel.movieItemPressed?.let { movie ->
-        Column(verticalArrangement = Arrangement.SpaceBetween,
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            Row(horizontalArrangement = Arrangement.Center) {
-                Text(text = movie.original_title,
-                    fontSize = 30.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            }
+    val isLoading: Boolean = viewModel.inLoadingState.value
 
-            Row(horizontalArrangement = Arrangement.Center) {
-                movie.largePosterImgBitmap?.let {
-                    Image(
-                        bitmap = it.asImageBitmap(),
-                        contentDescription = movie.original_title
+    viewModel.fetchMoviePoster()
+
+    viewModel.movieItemPressed?.let { movie ->
+        Box(modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            CircularProgressBarIndicator(shouldBeDisplayed = isLoading)
+            Column(verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                Row(horizontalArrangement = Arrangement.Center) {
+                    Text(text = movie.original_title,
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
                     )
                 }
-            }
 
-            Row(horizontalArrangement = Arrangement.Center) {
-                Text(text = movie.release_date, fontSize = 20.sp)
-            }
+                Row(horizontalArrangement = Arrangement.Center) {
+                    movie.largePosterImgBitmap?.let {
+                        Image(
+                            bitmap = it.asImageBitmap(),
+                            contentDescription = movie.original_title
+                        )
+                    }
+                }
 
-            Row(horizontalArrangement = Arrangement.Center) {
-                Text(text = movie.overview, fontSize = 15.sp)
+                Row(horizontalArrangement = Arrangement.Center) {
+                    Text(text = movie.release_date, fontSize = 20.sp)
+                }
+
+                Row(horizontalArrangement = Arrangement.Center) {
+                    Text(text = movie.overview, fontSize = 15.sp)
+                }
             }
         }
     }
