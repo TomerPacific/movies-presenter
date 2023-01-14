4cad82bd81d75468d7418549c9526accc43d68c0
unknown <tomerpacific@gmail.com>
2023-01-14 18:51:46 +0200

    MoviePresenter using logic to check if there is an internet connection before fetching data

--- app/src/main/java/com/tomerpacific/moviepresenter/model/MainViewModel.kt
+++ app/src/main/java/com/tomerpacific/moviepresenter/model/MainViewModel.kt
@@ -5,24 +5,33 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import com.tomerpacific.moviepresenter.network.NetworkConnectivityManager
 import com.tomerpacific.moviepresenter.repository.MovieRepositoryImpl
 import kotlinx.coroutines.launch
 
 class MainViewModel(application: Application): AndroidViewModel(application) {
 
     private val movieRepository: MovieRepositoryImpl = MovieRepositoryImpl()
+    private val networkConnectivityManager: NetworkConnectivityManager = NetworkConnectivityManager()
     val moviesList: MutableState<List<MovieModel>> = mutableStateOf(listOf())
     var inLoadingState: MutableState<Boolean> = mutableStateOf(true)
+    var isInternetConnectionAvailable: MutableState<Boolean> = mutableStateOf(true)
 
     var movieItemPressed: MovieModel? = null
 
     init {
-        viewModelScope.launch {
-            movieRepository.fetchMovies()?.let { response ->
-               var movies: List<MovieModel> = response.results
-               movies = movieRepository.fetchMoviePosters(movies)
-                moviesList.value = movies
-                inLoadingState.value = false
+
+        if (!networkConnectivityManager.isNetworkConnected(application.applicationContext)) {
+            inLoadingState.value = false
+            isInternetConnectionAvailable.value = false
+        } else {
+            viewModelScope.launch {
+                movieRepository.fetchMovies()?.let { response ->
+                    var movies: List<MovieModel> = response.results
+                    movies = movieRepository.fetchMoviePosters(movies)
+                    moviesList.value = movies
+                    inLoadingState.value = false
+                }
             }
         }
     }
