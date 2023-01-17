433e179fa841d9f98780f3384495e04631b1bd14
unknown <tomerpacific@gmail.com>
2023-01-17 21:36:40 +0200

    MoviePresenter removing unused constant and meta data tag

--- app/src/main/java/com/tomerpacific/moviepresenter/Constants.kt
+++ app/src/main/java/com/tomerpacific/moviepresenter/Constants.kt
@@ -1,7 +1,6 @@
 package com.tomerpacific.moviepresenter
 
 object Constants {
-    const val TMDB_META_DATA_KEY = "TMDB"
     const val POPULAR_MOVIES_ENDPOINT = "https://api.themoviedb.org/3/movie/popular?api_key="
     const val MOVIE_POSTER_ENDPOINT = "https://image.tmdb.org/t/p/"
     const val MOVIE_POSTER_SMALL_SIZE = "w200/"
