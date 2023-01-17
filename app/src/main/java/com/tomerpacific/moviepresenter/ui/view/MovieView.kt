c278aabe4889138be08a06b36c0d8a740e87abde
unknown <tomerpacific@gmail.com>
2023-01-17 21:31:07 +0200

    MoviePresenter adding star icons to movie rating

--- app/src/main/java/com/tomerpacific/moviepresenter/ui/view/MovieView.kt
+++ app/src/main/java/com/tomerpacific/moviepresenter/ui/view/MovieView.kt
@@ -1,13 +1,15 @@
 package com.tomerpacific.moviepresenter.ui.view
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -60,11 +62,19 @@ fun MovieView(viewModel: MainViewModel) {
                 }
 
                 Row(horizontalArrangement = Arrangement.Center) {
+                    Icon(modifier = Modifier.size(35.dp),
+                        imageVector = Icons.Rounded.Star,
+                        contentDescription = "Star Icon",
+                        tint = Color.Yellow)
                     Text(modifier = Modifier.padding(5.dp),
                         text = "Rating: " + movie.vote_average.toString(),
                         fontSize = 22.sp,
                         textAlign = TextAlign.Center,
                         color = Utils.getColorRating(movie.vote_average))
+                    Icon(modifier = Modifier.size(35.dp),
+                        imageVector = Icons.Rounded.Star,
+                        contentDescription = "Star Icon",
+                        tint = Color.Yellow)
                 }
             }
         }
