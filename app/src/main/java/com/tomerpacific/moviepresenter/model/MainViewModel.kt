78aa3d48baeaa2e28152951557169dec612480dc
unknown <tomerpacific@gmail.com>
2023-11-02 21:17:41 +0200

    MoviesPresenter converting livedata to stateflow

--- app/src/main/java/com/tomerpacific/moviepresenter/model/MainViewModel.kt
+++ app/src/main/java/com/tomerpacific/moviepresenter/model/MainViewModel.kt
@@ -2,13 +2,13 @@ package com.tomerpacific.moviepresenter.model
 
 import android.app.Application
 import android.graphics.Bitmap
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.tomerpacific.moviepresenter.cache.MovieImageCache
 import com.tomerpacific.moviepresenter.network.NetworkConnectivityManager
 import com.tomerpacific.moviepresenter.repository.MovieRepositoryImpl
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class MainViewModel(application: Application): AndroidViewModel(application) {
@@ -16,24 +16,30 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
     private val movieRepository: MovieRepositoryImpl = MovieRepositoryImpl()
     private val networkConnectivityManager: NetworkConnectivityManager = NetworkConnectivityManager()
     private val movieImageCache: MovieImageCache = MovieImageCache()
-    val moviesList: MutableState<List<MovieModel>> = mutableStateOf(listOf())
-    var inLoadingState: MutableState<Boolean> = mutableStateOf(true)
-    var isInternetConnectionAvailable: MutableState<Boolean> = mutableStateOf(true)
+
+    private val _moviesList: MutableStateFlow<List<MovieModel>> = MutableStateFlow(listOf())
+    val moviesList: StateFlow<List<MovieModel>> = _moviesList
+
+    private val _inLoadingState: MutableStateFlow<Boolean> = MutableStateFlow(true)
+    val inLoadingState: StateFlow<Boolean> = _inLoadingState
+
+    private val _isInternetConnectionAvailable: MutableStateFlow<Boolean> = MutableStateFlow(true)
+    val isInternetConnectionAvailable: StateFlow<Boolean> = _isInternetConnectionAvailable
 
     var movieItemPressed: MovieModel? = null
 
     init {
 
         if (!networkConnectivityManager.isNetworkConnected(application.applicationContext)) {
-            inLoadingState.value = false
-            isInternetConnectionAvailable.value = false
+            _inLoadingState.value = false
+            _isInternetConnectionAvailable.value = false
         } else {
             viewModelScope.launch {
                 movieRepository.fetchMovies()?.let { response ->
                     var movies: List<MovieModel> = response.results
                     movies = movieRepository.fetchMoviePosters(movies)
-                    moviesList.value = movies
-                    inLoadingState.value = false
+                    _moviesList.value = movies
+                    _inLoadingState.value = false
                 }
             }
         }
@@ -41,7 +47,7 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
 
     fun handleNavigationToMovieViewFromMovieCard(movie: MovieModel) {
         movieItemPressed = movie
-        inLoadingState.value = true
+        _inLoadingState.value = true
         fetchMoviePoster(movie)
     }
 
@@ -57,10 +63,10 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
                     movie.posterImgPath -> movieItemPressed!!.smallPosterImgBitmap = bitmap
                     else -> movieItemPressed!!.largeBackdropImgBitmap = bitmap
                 }
-                inLoadingState.value = false
+                _inLoadingState.value = false
             } ?: viewModelScope.launch {
                 movieItemPressed = movieRepository.fetchMoviePoster(movie)
-                inLoadingState.value = false
+                _inLoadingState.value = false
                 val bitmap: Bitmap? = when (imagePath) {
                     movie.posterImgPath -> movie.smallPosterImgBitmap
                     else -> movie.largeBackdropImgBitmap
