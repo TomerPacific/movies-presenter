a6746455320873b9bacd43c200a7c1b3f756cd65
unknown <tomerpacific@gmail.com>
2023-11-17 15:10:23 +0200

    MoviesPresenter creating constants for rating colors

--- app/src/main/java/com/tomerpacific/moviepresenter/Utils.kt
+++ app/src/main/java/com/tomerpacific/moviepresenter/Utils.kt
@@ -5,6 +5,12 @@ import androidx.compose.ui.graphics.Color
 class Utils {
 
     companion object {
+
+        val GREAT_RATING_COLOR = Color(40, 180, 99)
+        val AVERAGE_RATING_COLOR = Color(212, 172, 13)
+        val POOR_RATING_COLOR = Color(203, 67, 53)
+
+
         fun reverseDateFormat(date: String):String {
             val splitDate: List<String> = date.split("-")
             return splitDate[2] + "-" + splitDate[1] + "-" + splitDate[0]
@@ -12,9 +18,9 @@ class Utils {
 
         fun getColorRating(movieRating: Double): Color {
             return when(movieRating) {
-                 in 8.0..10.0 -> Color(40, 180, 99)
-                 in 6.5..7.9 -> Color(212, 172, 13)
-                 else -> Color(203, 67, 53)
+                 in 8.0..10.0 -> GREAT_RATING_COLOR
+                 in 6.5..7.9 -> AVERAGE_RATING_COLOR
+                 else -> POOR_RATING_COLOR
             }
         }
     }
