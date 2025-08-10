f6bdb0685126bd151e37162b5bda14cbbd8143e3
unknown <tomerpacific@gmail.com>
2025-08-10 10:07:31 +0300

    MoviesPresenter start value of isLoading is true

--- app/src/main/java/com/tomerpacific/moviepresenter/model/MainUiState.kt
+++ app/src/main/java/com/tomerpacific/moviepresenter/model/MainUiState.kt
@@ -1,7 +1,7 @@
 package com.tomerpacific.moviepresenter.model
 
 data class MainUiState(
-    val isLoading: Boolean = false,
+    val isLoading: Boolean = true,
     val moviesList: List<MovieModel>? = null,
     val isInternetConnectionAvailable: Boolean = true,
     val movieItemPressed: MovieModel? = null,
