2a174589f957477d5658cf16f735d70cb3304f66
unknown <tomerpacific@gmail.com>
2025-08-10 10:19:13 +0300

    MoviesPresenter creating class composable for movie list

---
+++ app/src/main/java/com/tomerpacific/moviepresenter/ui/view/MovieList.kt
@@ -0,0 +1,188 @@
+package com.tomerpacific.moviepresenter.ui.view
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContent
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.tomerpacific.moviepresenter.model.MainViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+private const val APP_TITLE = "Movies Presenter"
+private const val itemIndexToShowScrollToTopButton: Int = 10
+
+@Composable
+fun MovieList(mainViewModel: MainViewModel,
+              onNavigateToMovieView: () -> Unit) {
+
+    val mainUiState by mainViewModel.mainUiState.collectAsState()
+    val lazyListState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    val userReachedBottomOfColumn =
+        didUserReachBottomOfColumn(lazyListState = lazyListState, bufferFromBottom = 3)
+
+    LaunchedEffect(userReachedBottomOfColumn) {
+        if (userReachedBottomOfColumn) {
+            mainViewModel.fetchMoreMovies()
+        }
+    }
+
+    Scaffold(
+        contentWindowInsets = WindowInsets.safeContent
+    ) { innerPadding ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = APP_TITLE,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    state = lazyListState,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    when {
+                        mainUiState.isLoading || userReachedBottomOfColumn -> {
+                            item {
+                                CircularProgressBarIndicator()
+                            }
+                        }
+
+                        !mainUiState.isInternetConnectionAvailable -> {
+                            item {
+                                NetworkErrorText()
+                            }
+                        }
+
+                        mainUiState.moviesList?.isNotEmpty()!! -> {
+                            items(mainUiState.moviesList!!) { movie ->
+                                MovieCard(
+                                    movie = movie,
+                                    viewModel = mainViewModel,
+                                    onNavigateToMovieView = onNavigateToMovieView
+                                )
+                            }
+                        }
+                    }
+                }
+                ScrollToTopButton(coroutineScope, listState = lazyListState)
+            }
+        }
+    }
+}
+
+@Composable
+fun NetworkErrorText() {
+    Text(text = "There is no internet connection. Please check it and try again.",
+        fontSize = 25.sp,
+        textAlign = TextAlign.Center)
+}
+
+@Composable
+fun didUserReachBottomOfColumn(lazyListState: LazyListState, bufferFromBottom: Int): Boolean {
+    val isUserAtBottomOfColumn by remember {
+        derivedStateOf {
+            val layoutInfo = lazyListState.layoutInfo
+            val visibleItemsInfo = layoutInfo.visibleItemsInfo
+            if (layoutInfo.totalItemsCount <= 1) {
+                false
+            } else {
+                val lastVisibleItem = visibleItemsInfo.last()
+
+                lastVisibleItem.index >= layoutInfo.totalItemsCount - 1 - bufferFromBottom
+            }
+        }
+    }
+
+    return isUserAtBottomOfColumn
+}
+
+@Composable
+fun ScrollToTopButton(coroutineScope: CoroutineScope, listState: LazyListState) {
+    var scrollToTopButtonVisibility by remember {
+        mutableStateOf(false)
+    }
+
+    scrollToTopButtonVisibility = shouldShowScrollTopTopButton(listState)
+
+    AnimatedVisibility(visible = scrollToTopButtonVisibility,
+        enter = fadeIn(),
+        exit = fadeOut()
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            IconButton(
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .background(Color.White, CircleShape)
+                    .then(Modifier.size(50.dp))
+                    .border(3.dp, Color.Black, shape = CircleShape),
+                onClick = {
+                    coroutineScope.launch {
+                        listState.animateScrollToItem(0)
+                    }
+                }) {
+                Icon(Icons.Filled.KeyboardArrowUp, contentDescription = "Up arrow")
+            }
+        }
+    }
+}
+
+private fun shouldShowScrollTopTopButton(listState: LazyListState): Boolean {
+    return listState.firstVisibleItemIndex >= itemIndexToShowScrollToTopButton
+}
