ea7f65156b2a632ebadaded69242e128073ed574
unknown <tomerpacific@gmail.com>
2025-08-15 20:51:06 +0300

    MoviesPresenter creating view model factory

---
+++ app/src/main/java/com/tomerpacific/moviepresenter/model/MainViewModelFactory.kt
@@ -0,0 +1,22 @@
+package com.tomerpacific.moviepresenter.model
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.tomerpacific.moviepresenter.domain.usecase.GetMoviePosterUseCase
+import com.tomerpacific.moviepresenter.domain.usecase.GetMoviePostersUseCase
+import com.tomerpacific.moviepresenter.domain.usecase.GetMoviesUseCase
+
+class MainViewModelFactory(private val application: Application,
+                           private val getMoviesUseCase: GetMoviesUseCase,
+                           private val getMoviePostersUseCase: GetMoviePostersUseCase,
+                           private val getMoviePosterUseCase: GetMoviePosterUseCase): ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return MainViewModel(application, getMoviesUseCase, getMoviePostersUseCase, getMoviePosterUseCase) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
