eed32e4225d89b2222dee34218d8440aa6785c15
unknown <tomerpacific@gmail.com>
2025-08-15 20:51:25 +0300

    MoviesPresenter updating usecases

--- app/src/main/java/com/tomerpacific/moviepresenter/domain/usecase/GetMoviePosterUseCase.kt
+++ app/src/main/java/com/tomerpacific/moviepresenter/domain/usecase/GetMoviePosterUseCase.kt
@@ -1,4 +1,9 @@
 package com.tomerpacific.moviepresenter.domain.usecase
 
-class GetMoviePosterUseCase {
+import com.tomerpacific.moviepresenter.domain.model.MovieModel
+import com.tomerpacific.moviepresenter.domain.repository.MovieRepository
+
+class GetMoviePosterUseCase(private val repository: MovieRepository) {
+
+    suspend operator fun invoke(movie: MovieModel) = repository.fetchMoviePoster(movie)
 }
--- app/src/main/java/com/tomerpacific/moviepresenter/domain/usecase/GetMoviesUseCase.kt
+++ app/src/main/java/com/tomerpacific/moviepresenter/domain/usecase/GetMoviesUseCase.kt
@@ -1,4 +1,7 @@
 package com.tomerpacific.moviepresenter.domain.usecase
 
-class GetMoviesUseCase {
+import com.tomerpacific.moviepresenter.domain.repository.MovieRepository
+
+class GetMoviesUseCase(private val repository: MovieRepository) {
+    suspend operator fun invoke() = repository.fetchMovies()
 }
